48d512b1f9b0c29989057948d08074371d783b9b
justinemar <jmarcantado@gmail.com>
2022-05-21 01:27:03 +0800

    bootstrap initial client side with svelte

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+import MyAxies from "./components/MyAxies.svelte";
+import TopWallets from "./components/TopWallets.svelte";
+import Wallet from "./components/Wallet.svelte";
+
+	export let name: string;
+</script>
+
+<main>
+	<header>
+		<div class="logo"></div>
+	</header>
+
+	<div class="section-container">
+		<section>
+			<Wallet/>
+			<MyAxies/>
+			<TopWallets/>
+		</section>
+		<section>
+			<h3>Recent Records</h3>
+		</section>
+	</div>
+
+</main>
+
+<style>
+	.section-container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	section {
+		margin: 10px;
+		flex-grow: 1;
+	}
+	.logo {
+		background-image: url("/images/AXIEFLOOR.png");
+		width: 100%;
+		height: 5rem;
+		background-repeat: no-repeat;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/MyAxies.svelte
@@ -0,0 +1,49 @@
+<script>
+
+</script>
+
+
+
+
+<div class="container my-axies-container">
+    <div class="wrapper my-axie">
+        <p>My Axies <span class="p-highlight">32</span></p>
+        <div class="axie-list">
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+            <div class="axie"></div>
+        </div>
+
+        <p>Total Floor Worth <span class="p-highlight">300</span> USD</p>
+    </div>
+</div>
+
+
+<style>
+
+    .axie-list {
+        /* display: flex;
+        flex-direction: row; */
+        overflow: scroll;
+        display: block;
+        width: 500px;
+        max-height: 200px;
+    }
+
+    .axie {
+        width: 120px;
+        height: 100px;
+        background: #2B445A;
+        margin: 5px;
+        display: inline-block;
+    }
+</style>
